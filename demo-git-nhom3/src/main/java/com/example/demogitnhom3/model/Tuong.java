9c1ea82e67e9b16da734181382bf57987a081b28
Tuong <aumanhtuong2001@gmail.com>
2023-03-10 13:46:26 +0700

    hehe

--- demo-git-nhom3/src/main/java/com/example/demogitnhom3/model/Tuong.java
+++ demo-git-nhom3/src/main/java/com/example/demogitnhom3/model/Tuong.java
@@ -3,10 +3,6 @@ package com.example.demogitnhom3.model;
 public class Tuong {
     private String name;
     private int id;
-private int hehehehehehehehehehe;
-private String aaaaaaaaaaaaaaaaaa;
-
-    private double point;
 
 
 }
