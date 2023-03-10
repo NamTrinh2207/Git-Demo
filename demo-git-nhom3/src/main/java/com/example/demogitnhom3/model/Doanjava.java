28838c6bf37100cd04a7b8ad4d17290082353dca
lê đoàn <huongly95967@gmail.com>
2023-03-10 11:48:26 +0700

    add connection

--- demo-git-nhom3/src/main/java/com/example/demogitnhom3/model/Doanjava.java
+++ demo-git-nhom3/src/main/java/com/example/demogitnhom3/model/Doanjava.java
@@ -1,4 +1,27 @@
 package com.example.demogitnhom3.model;
 
-public class Doanjava {
-}
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+    public class Doanjava {
+        public Connection getConnection() {
+            String jdbcURL = "jdbc:mysql://localhost:3306/bth1jdbc";
+            String jdbcUsername = "root";
+            String jdbcPassword = "10061";
+            Connection connection = null;
+            try {
+                Class.forName("com.mysql.jdbc.Driver");
+                connection = DriverManager.getConnection(jdbcURL,jdbcUsername,jdbcPassword);
+                System.out.println("connection successfully");
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return connection;
+        }
+    }
+
+
