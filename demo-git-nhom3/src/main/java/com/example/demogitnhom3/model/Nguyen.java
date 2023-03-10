b4cb5801cf43f29bb07bc4cbd7ac80cd5377655c
dinhnguyen96 <39048935+dinhnguyen96@users.noreply.github.com>
2023-03-10 10:44:33 +0700

    update Nguyen

--- demo-git-nhom3/src/main/java/com/example/demogitnhom3/model/Nguyen.java
+++ demo-git-nhom3/src/main/java/com/example/demogitnhom3/model/Nguyen.java
@@ -1,4 +1,23 @@
 package com.example.demogitnhom3.model;
 
 public class Nguyen {
+
+    private String name;
+
+    public Nguyen()
+    {
+
+    }
+
+    public Nguyen(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
